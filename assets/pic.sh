d5939c669d3a56f1f41fd9e88acc65f99a3b8bfa
jiaojiaoloveu <jiaojiaoloveu@163.com>
2016-08-17 17:10:17 +0800

    add new images

--- assets/pic.sh
+++ assets/pic.sh
@@ -2,8 +2,11 @@
 for file in ./pic/*
 do 
     newfile="./thumbnail/$(basename $file)"
+    newfile2="./markpic/$(basename $file)"
     if [ ! -f $newfile ]; then
         convert -resize 200x200 $file $newfile
-        convert $file icon.png -gravity southeast -geometry +5+10 -composite $file
+    fi
+    if [ ! -f $newfile2 ]; then
+        convert $file icon.png -gravity southeast -geometry +5+10 -composite $newfile2
     fi
 done
