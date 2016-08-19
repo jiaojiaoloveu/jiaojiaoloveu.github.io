113b2107b5e11aaa236d5987bfd94031b1e68a77
jiaojiaoloveu <jiaojiaoloveu@163.com>
2016-08-19 16:32:13 +0800

    test

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #/bin/bash
 echo $1
+cd assets
+sh pic.sh
+echo "finish pics"
+cd ..
 jekyll build
+echo "build finish"
 git add .
 git commit -m $1
 git push -u origin source
+echo "source push finish"
 cd _site
 git add .
 git commit -m $1
 git push -u origin master
+echo "master push finish"
